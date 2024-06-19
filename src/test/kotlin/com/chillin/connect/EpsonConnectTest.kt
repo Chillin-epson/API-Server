4b01c1ef01c2fca75eb32c9bfe5d24e660a520b1
zionhann <hanzion1108@gmail.com>
2024-06-19 13:45:12 +0900

    test: 프린트 API 관련 테스트 코드 추가

--- src/test/kotlin/com/chillin/connect/EpsonConnectTest.kt
+++ src/test/kotlin/com/chillin/connect/EpsonConnectTest.kt
@@ -1,5 +1,6 @@
 package com.chillin.connect
 
+import com.chillin.connect.request.PrintSettingsRequest
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.core.test.TestCase
 import io.kotest.core.test.TestResult
@@ -7,6 +8,7 @@ import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.redis.core.StringRedisTemplate
+import org.springframework.http.MediaType
 
 @SpringBootTest
 class EpsonConnectTest(
@@ -17,6 +19,7 @@ class EpsonConnectTest(
     given("인증을 한번도 하지 않았을 때") {
         `when`("인증을 시도해 성공하면") {
             val accessToken = epsonConnectApi.authenticate()
+
             then("레디스에 액세스 토큰이 저장되어야 한다.") {
                 val redisAccessToken = redisTemplate.opsForValue().get("accessToken")
 
@@ -31,6 +34,7 @@ class EpsonConnectTest(
         `when`("토큰이 만료된 후 인증을 한번 더 시도하면") {
             redisTemplate.opsForValue().getAndDelete("accessToken")
             epsonConnectApi.authenticate()
+
             then("만료된 토큰과 새로 발급된 토큰이 달라야 한다.") {
                 val redisAccessToken = redisTemplate.opsForValue().get("accessToken")
 
@@ -43,6 +47,7 @@ class EpsonConnectTest(
     given("인증을 이미 했을 때") {
         `when`("토큰이 만료되지 않고 인증을 한번 더 시도하면") {
             val accessToken = epsonConnectApi.authenticate()
+
             then("두 토큰은 같아야 한다.") {
                 accessToken shouldBe epsonConnectApi.authenticate()
             }
@@ -50,8 +55,11 @@ class EpsonConnectTest(
     }
 
     given("기본 프린트 설정으로") {
+        val defaultSettings = PrintSettingsRequest()
+
         `when`("값을 설정하면") {
-            val response = epsonConnectApi.setPrintSettings()
+            val response = epsonConnectApi.setPrintSettings(defaultSettings)
+
             then("Job ID와 업로드 URI가 생성되어야 한다.") {
                 response.id shouldNotBe ""
                 response.uploadUri shouldNotBe ""
@@ -60,13 +68,29 @@ class EpsonConnectTest(
     }
 
     given("프린트 설정한 뒤") {
-        val (_, uploadUri) = epsonConnectApi.setPrintSettings()
+        val defaultSettings = PrintSettingsRequest()
+        val (_, uploadUri) = epsonConnectApi.setPrintSettings(defaultSettings)
 
         `when`("업로드를 시도하면") {
             val fileData = ByteArray(0)
-            val response = epsonConnectApi.uploadFileToPrint(uploadUri, fileData)
+            val isSuccessful = epsonConnectApi.uploadFileToPrint(uploadUri, fileData, MediaType.IMAGE_JPEG_VALUE)
+
             then("업로드가 성공해야 한다.") {
-                response.code shouldBe 200
+                isSuccessful shouldBe true
+            }
+        }
+    }
+
+    given("프린트할 파일이 있을 때") {
+        val fileData = ByteArray(0)
+        // or use File("path/to/document").readBytes() to print an actual document
+
+        `when`("프린트를 시도하면") {
+            val defaultSettings = PrintSettingsRequest()
+            val isSuccessful = epsonConnectApi.print(fileData, MediaType.IMAGE_JPEG_VALUE, defaultSettings)
+
+            then("프린트가 성공해야 한다.") {
+                isSuccessful shouldBe true
             }
         }
     }
