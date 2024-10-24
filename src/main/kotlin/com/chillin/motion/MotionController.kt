9fbefb5e6c434dd4307cfc753c7aa0a692b53690
zionhann <hanzion1108@gmail.com>
2024-10-24 18:43:41 +0900

    fix: 모션 적용 이미지 계정별 관리하도록 수정

--- src/main/kotlin/com/chillin/motion/MotionController.kt
+++ src/main/kotlin/com/chillin/motion/MotionController.kt
@@ -2,12 +2,14 @@ package com.chillin.motion
 
 import com.chillin.drawing.DrawingService
 import com.chillin.drawing.response.DrawingResponse
+import com.chillin.member.MemberService
 import com.chillin.motion.request.MotionRequest
 import com.chillin.s3.S3Service
 import com.chillin.type.DrawingType
 import com.chillin.type.MediaSubtype
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -20,17 +22,23 @@ import java.util.*
 class MotionController(
     private val drawingService: DrawingService,
     private val s3Service: S3Service,
-    private val motionService: MotionService
+    private val motionService: MotionService,
+    private val memberService: MemberService
 ) {
     @PostMapping("/{drawingId}")
-    fun addMotion(@PathVariable drawingId: Long, @RequestBody request: MotionRequest): ResponseEntity<DrawingResponse> {
+    fun addMotion(
+        @PathVariable drawingId: Long,
+        @RequestBody request: MotionRequest,
+        @AuthenticationPrincipal accountId: String
+    ): ResponseEntity<DrawingResponse> {
         val srcPathname = drawingService.getNameById(drawingId)
         val (jpegBytes, _) = s3Service.getImageData(srcPathname)
         val gifBytes = motionService.addMotion(srcPathname, jpegBytes, request.motionType)
 
         val targetPathname = "animated/${UUID.randomUUID()}.${MediaSubtype.GIF.value}"
         val (uploadedFilePathname, url) = s3Service.uploadImage(targetPathname, gifBytes)
-        val savedImage = drawingService.save(uploadedFilePathname, DrawingType.ANIMATED)
+        val member = memberService.findMemberByAccountId(accountId)
+        val savedImage = drawingService.save(member, uploadedFilePathname, DrawingType.ANIMATED)
 
         val response = DrawingResponse(savedImage.drawingId, url)
         return ResponseEntity.status(HttpStatus.CREATED).body(response)
